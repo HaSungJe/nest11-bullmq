67d4d7bdb4aa9fecac4e4e1ce58f724205c5c4b4
HaSeongJe <timeclas3@gmail.com>
2026-04-03 08:35:30 +0900

    2026.04.03
    - fcm 발송실패 반환 정보 변경

--- src/modules/firebase-cloud-message/firebase-cloud-message.service.ts
+++ src/modules/firebase-cloud-message/firebase-cloud-message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -22,44 +22,28 @@ export class FirebaseCloudeMessageService {
      * @param body 
      * @param os 
      */
-    async sendFirebaseCloudMessage(token: string, title: string, body: string, os: string): Promise<object> {
+    async sendFirebaseCloudMessage(token: string, title: string, body: string, os: string): Promise<void> {
         const message: admin.messaging.Message = {
             token,
-            notification: {
-                title,
-                body,
-            },
+            notification: {title, body}
         };
 
         if (os === 'android') {
             message.android = {
-                notification: {
-                    sound: 'default',
-                },
+                notification: {sound: 'default'},
                 priority: 'high',
             };
         } else if (os === 'ios') {
             message.apns = {
-                payload: {
-                    aps: {
-                        alert: {
-                            title,
-                            body,
-                        },
-                        sound: 'default',
-                    },
-                },
-                headers: {
-                    'apns-priority': '10',
-                },
+                payload: {aps: {alert: {title, body}, sound: 'default'}},
+                headers: {'apns-priority': '10'},
             };
         }
 
         try {
             await admin.messaging().send(message);
-            return { statusCode: 200 }
         } catch (error) {
-            return { statusCode: 400, message: '앱 푸쉬 발송 실패.', error_code: error.errorInfo.code,  error_message: error.errorInfo.message }
+            throw new BadRequestException('firebase-cloud-message 발송에 실패하였습니다.');
         }
     }
 }
